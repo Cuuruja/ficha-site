dc5600d69fda8e36162473617f2b3479058698c1
Pedro Arthur <127543180+Cuuruja@users.noreply.github.com>
2024-05-22 15:45:33 -0300

    Add files via upload

--- pages/registro.php
+++ pages/registro.php
@@ -32,20 +32,29 @@
         </div>
       </nav>
     
-
-<main class="conteudo d-flex justify-content-center align-items-center min-vh-100" id="conteudo">
+<main class="conteudo d-flex justify-content-center align-items-center min-vh-100 " id="conteudo">
     <div class="card p-4" style="width: 100%; max-width: 400px;">
         <h2 class="text-center mb-4">Cadastre-se</h2>
         <form id="formRegistro" action="registro.php" method="post">
             <div class="form-group">
-                <label for="usuarioRegistro">Usuário:</label>
-                <input type="text" id="usuarioRegistro" name="usuarioRegistro" class="form-control" placeholder="Usuário" required>
+                <label for="usuario">Usuário:</label>
+                <input type="text" id="usuario" name="usuario" class="form-control" placeholder="Usuário" required>
             </div>
             <div class="form-group">
-                <label for="senhaRegistro">Senha:</label>
-                <input type="password" id="senhaRegistro" name="senhaRegistro" class="form-control" placeholder="Senha" required>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" class="form-control" placeholder="Email" required>
             </div>
-            <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+            <div class="form-group">
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" class="form-control" placeholder="Senha" required>
+            </div>
+            <div class="form-group">
+                <label for="confirm_senha">Repita sua Senha:</label>
+                <input type="password" id="confirm_senha" name="confirm_senha" class="form-control" placeholder="Repita sua senha" required>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary btn-block">Registrar</button>
+            </div>
         </form>
     </div>
 </main>
@@ -55,3 +64,23 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
+<?php
+include_once '../php/conexao.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nome = $_POST['usuario'];
+  $email = $_POST['email'];
+  $senha = $_POST['senha'];
+
+  $senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
+
+  $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$nome', '$email', '$senhaCriptografada')";
+
+  if ($conn->query($sql) === TRUE) {
+      echo "Cadastro realizado com sucesso.";
+  } else {
+      echo "Erro ao cadastrar: " . $conn->error;
+  }
+}
+
+?>
